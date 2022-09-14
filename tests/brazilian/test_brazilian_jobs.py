74a0d1e99bba57de743bac3bc98c3dbefc0ac1fa
hector <hectoreddbenitez@gmail.com>
2022-09-14 20:19:32 -0300

    feat: req11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
+
+mock_result = {
+    "title": "Analista de Software",
+    "salary": "4000",
+    "type": "full time",
+}
+path = "tests/mocks/brazilians_jobs.csv"
 
 
 def test_brazilian_jobs():
-    pass
+    assert mock_result in read_brazilian_file(path)
