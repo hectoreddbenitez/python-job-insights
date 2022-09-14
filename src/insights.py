78d9c342a4e532d1703b60c40377674a1c541ad5
hector <hectoreddbenitez@gmail.com>
2022-09-14 11:00:35 -0300

    req08

--- src/insights.py
+++ src/insights.py
@@ -55,7 +55,7 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """refatoração, Ó que lindo!!"""
+    """refatoração, Ó que"""
     jobs_list = read(path)
     salaries_list = [
         int(job["min_salary"])
@@ -72,29 +72,15 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    """https://docs.python.org/pt-br/3/tutorial/errors.html"""
+    try:
+        if job["min_salary"] <= int(salary) <= job["max_salary"]:
+            return True
+        if job["min_salary"] > job["max_salary"]:
+            raise ValueError("não é assim não...")
+        return False
+    except BaseException as err:
+        raise err and ValueError
 
 
 def filter_by_salary_range(jobs, salary):
